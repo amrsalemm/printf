561fdc1a127d786c961e920b07317e73de750fb0
amrsalemm <amrsalemmashaal21@gmail.com>
2023-10-21 02:00:10 +0300

    updated

--- main.h
+++ main.h
@@ -1,40 +1,36 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct format - Struct for format
- * @ch: Struct format
- * @f: The function associated
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
  */
-
-typedef struct format
+typedef struct structprint
 {
-	char ch;
-	int (*f)(va_list);
-} format_t;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
-void print_Hexadecimal(unsigned int num, int *p);
-int print_ex_str(va_list ap);
-int print_hex_ptr(va_list ap);
-int print_address(va_list ap);
-int print_binary(va_list ap);
-int print_unsigned(va_list ap);
-int print_octal(va_list ap);
-int print_hex(va_list ap);
-int print_Hex(va_list ap);
-int reverse_string(va_list ap);
-int len_int(int ap);
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_char(va_list ap);
-int print_mod(va_list ap);
-int print_int(va_list ap);
-int print_string(va_list ap);
-
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
